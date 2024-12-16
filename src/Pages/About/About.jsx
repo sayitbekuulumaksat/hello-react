9ea65db1e68da0184774ef062d72f2b7b6d17429
sayitbekuulumaksat <sayitbekuulumaksat@gmail.com>
2024-12-16 17:56:32 +0600

    use the ityped

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { useRef, useEffect } from "react";
+import { init } from 'ityped'
 
 function About() {
+  const animateTextRef = useRef();
+  useEffect(() => {
+    init(animateTextRef.current, {
+      showCursor: true,
+      strings: ["Frontend React разработчик"],
+      backDelay: 1500,
+      backSpeed: 60,
+    });
+  }, []);
+
   return (
     <section className="about">
-      <h2>About</h2>
+      <h2 ref={animateTextRef}></h2>
     </section>
   );
 }
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import "./home.scss";
+import {useState} from "react";
 
 function Home() {
+  const [count, setCount]= useState(0)
+  function decrease(){
+    if(count> 0){
+      setCount(count-1)
+    }
+  }
   return (
     <section className="home">
-      <h2>Home</h2>
+      <div className="block">
+        <button onClick={decrease} className="toLeft">Left</button>
+        <p className="count">Here:  {count} </p>
+        <button onClick={()=>setCount( count + 1)} className="toRight">Right</button>
+      </div>
     </section>
   );
 }
